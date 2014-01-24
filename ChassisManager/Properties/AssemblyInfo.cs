cf8ae0c239b4ed5801eff326ef68dda12f541114
Bikash Sharma <bsharma@microsoft.com>
2014-01-24 11:34:14 -0800

    Update AssemblyInfo.cs

--- ChassisManager/Properties/AssemblyInfo.cs
+++ ChassisManager/Properties/AssemblyInfo.cs
@@ -10,4 +10,43 @@
 // CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License. 
 
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WCS Chassis Manager Service")]
+[assembly: AssemblyDescription("Windows Cloud Server Chassis Manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Windows Cloud Server")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("WcsTestUtil")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b50bb5a-cc99-40d6-a741-ae90cfd24c0f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("3.0.0.3")]
+//[assembly: AssemblyFileVersion("3.0.0.3")]
+
+
 
