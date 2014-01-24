ac40cad564874282852b4ee149096a416829bc97
Bikash Sharma <bsharma@microsoft.com>
2014-01-24 11:38:57 -0800

    Update CircularTraceListener.cs

--- ChassisManager/Tracer/CircularTraceListener.cs
+++ ChassisManager/Tracer/CircularTraceListener.cs
@@ -10,6 +10,11 @@
 // CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License. 
 
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
 namespace Microsoft.GFS.WCS.ChassisManager
 {
     /// <summary>
