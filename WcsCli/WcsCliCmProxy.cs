9c3fc72b71d0fe9670c1bdb10d8d163da7e3199c
Bikash Sharma <bsharma@microsoft.com>
2014-01-24 11:00:24 -0800

    Update WcsCliCmProxy.cs

--- WcsCli/WcsCliCmProxy.cs
+++ WcsCli/WcsCliCmProxy.cs
@@ -10,6 +10,9 @@
 // CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License. 
 
+using System;
+using System.Collections.Generic;
+
 namespace Microsoft.GFS.WCS.WcsCli
 {
     internal static class WcsCliCmProxy
