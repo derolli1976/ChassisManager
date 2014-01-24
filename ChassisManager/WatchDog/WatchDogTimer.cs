8a9052f6bd75b3bfd965af038ff9832383ebbac6
Bikash Sharma <bsharma@microsoft.com>
2014-01-24 11:39:36 -0800

    Update WatchDogTimer.cs

--- ChassisManager/WatchDog/WatchDogTimer.cs
+++ ChassisManager/WatchDog/WatchDogTimer.cs
@@ -10,6 +10,8 @@
 // CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License. 
 
+using System;
+
 namespace Microsoft.GFS.WCS.ChassisManager
 {
     /// <summary>
